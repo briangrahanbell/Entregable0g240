efaf32d5cea95e7e02457f1170a24fdb77e66ce5
Brian Grahan-Bell <110423587+briangrahanbell@users.noreply.github.com>
2022-08-18 10:44:26 -0300

    Jueves 18

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
+function cerrar(){
+        localStorage.removeItem('user');
+        location.href="login.html";
+}
+
+
 document.addEventListener("DOMContentLoaded", function(){
     //cuando apenas ingreso 
-    let usuario = sessionStorage.getItem("user");
+    let usuario = localStorage.getItem("user");
     if(usuario == null) {
         alert("Es necesario leguearse");
         location.href = "login.html";
-    } 
-
+    }
 
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
@@ -18,5 +23,8 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("muebles").addEventListener("click", function() {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
+    });
+    document.getElementById("cerrarBtn").addEventListener("click", function() {
+        cerrar();
     });
 });
--- js/login.js
+++ js/login.js
@@ -2,12 +2,16 @@ function login(){
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
 
-    if(email === ""){
-        alert("ingrese su email");
-    }else if(password === ""){
-        alert("ingrese su contraseña");
+    if(email === "" && password === ""){
+        document.getElementById('email').classList.add('is-invalid');
+        document.getElementById('password').classList.add('is-invalid');
+    }else if(email === ""){
+        document.getElementById('email').classList.add('is-invalid');
+    }
+    else if(password === ""){
+        document.getElementById('password').classList.add('is-invalid');
     }else{
-        sessionStorage.setItem('user',email);
+        localStorage.setItem('user',email);
         location.href="index.html";
     }
 }
